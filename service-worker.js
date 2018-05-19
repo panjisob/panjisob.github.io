dcca204e2bd97d4a1e47d2381df153638e88df94
Panji Sobari S <sobaripanji8@gmail.com>
2018-05-20 01:01:49 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,42 @@
 var cacheName = 'Barbershop-v1';
 var filesToCache = [
     '.',
+    'index.php',
     'index.html',
-    'assets/css/style.css',
-    'assets/css/bootstrap-theme.css',
-    'assets/css/bootstrap-theme.css.map',
-    'assets/css/bootstrap-theme.min.css',
-    'assets/css/bootstrap-theme.min.css.map',
-    'assets/css/bootstrap.css',
-    'assets/css/bootstrap.css.map',
-    'assets/css/bootstrap.min.css',
-    'assets/css/bootstrap.min.css.map',
-    'assets/fonts/glyphicons-halflings-regular.eot',
-    'assets/fonts/glyphicons-halflings-regular.svg',
-    'assets/fonts/glyphicons-halflings-regular.ttf',
-    'assets/fonts/glyphicons-halflings-regular.woff',
-    'assets/fonts/glyphicons-halflings-regular.woff2',
-    'assets/js/bootstrap.js',
-    'assets/js/bootstrap.min.js',
-    'assets/js/jquery.js',
-    'assets/js/npm.js',
+    'css/bootstrap.css',
+    'css/bootstrap.min.css',
+    'css/mdb.css',
+    'css/mdb.min.css',
+    'css/style.css',
+    'css/style.min.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    //'https://maps.googleapis.com/maps/api/js?key=AIzaSyAiwtjlLUES51ioTbDvTh07JRLmpwWtyYM&callback=initMap',
     'images/1.png',
     'images/2.png',
     'images/3.png',
+    'images/5216014_41a09f51-ca90-4500-8d86-a28a02cb49d9.jpg',
+    'images/132631072209210.jpg',
+    'images/barbershop.jpg',
+    'images/barberspomade.JPG',
+    'images/Classic_Barbershop_Beard_Oil_2_FL_OZ_3v_Pure_ONE_Beauty_1024x1024.jpg',
+    'images/Cultusia_Hair_Color_Shampoo_DARK_BROWN.jpg',
+    'images/Cultusia_Hair_Shampoo_.jpg',
     'images/home.jpg',
     'images/home1.jpg',
-    'images/hom.jpg'
+    'images/home2.jpg',
+    'images/p1.jpg',
+    'images/p2.jpg',
+    'images/p3.jpg',
+    'images/p4.jpg',
+    'images/p5.jpg',
+    'images/p6.jpg',
+    'js/bootstrap.js',
+    'js/bootstrap.min.js',
+    'js/jquery-3.2.1.min.js',
+    'js/mdb.js',
+    'js/mdb.min.js',
+    'js/popper.min.js'
+
 ];
 
 self.addEventListener('install', function(e) {
@@ -53,8 +64,10 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
 
+
 self.addEventListener('fetch', function(e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
+
     e.respondWith(
         caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
